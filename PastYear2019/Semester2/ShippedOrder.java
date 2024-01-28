967a821596bd14bdd9876cc4d48209dd2729249c
Xiang115 <149037049+Xiang115@users.noreply.github.com>
2024-01-28 23:18:30 +0800

    Update ShippedOrder.java

--- PastYear2019/Semester2/ShippedOrder.java
+++ PastYear2019/Semester2/ShippedOrder.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pastyear_2019_2;
 
-/**
- *
- * @author Goh
- */
 public class ShippedOrder extends Order {
 
     public ShippedOrder(String a, String b, int c, int d) {
