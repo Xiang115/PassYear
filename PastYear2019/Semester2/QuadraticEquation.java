fba470a1bf795069a74bc07226555cd46300c94d
Xiang115 <149037049+Xiang115@users.noreply.github.com>
2024-01-28 23:12:59 +0800

    Update QuadraticEquation.java

--- PastYear2019/Semester2/QuadraticEquation.java
+++ PastYear2019/Semester2/QuadraticEquation.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pastyear_2019_2;
 
-/**
- *
- * @author Goh
- */
 public class QuadraticEquation {
 
     private int a, b, c;
