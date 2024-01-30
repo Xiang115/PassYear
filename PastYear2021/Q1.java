4621f6a2601caf5ba8c983cbd1815a25aaa988f2
Xiang115 <149037049+Xiang115@users.noreply.github.com>
2024-01-30 18:19:55 +0800

    Add files via upload

--- PastYear2021/Q1.java
+++ PastYear2021/Q1.java
@@ -1,1 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pastyear_2022_1;
 
+/**
+ *
+ * @author Goh
+ */
+import java.util.Random;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.print("3 random price values: ");
+        RandomPrice(r);
+        System.out.print("5 random even-numbered years: ");
+        RandomEvenYear(r);
+        System.out.print("Car Plate Number: ");
+        RandomCarPlate(r);
+        System.out.print("Random Word : ");
+        RandomWord(r);
+    }
+
+    public static void RandomPrice(Random r) {
+        final double MAX = 300;
+        final double MIN = 100;
+        double[] Prices = new double[3];
+        for (int i = 0; i < Prices.length; i++) {
+            Prices[i] = r.nextDouble(MAX - MIN + 1) + MIN;
+            System.out.printf("%.2f ", Prices[i]);
+        }
+        System.out.println("");
+    }
+
+    public static void RandomEvenYear(Random r) {
+        final int MAX = 2030;
+        final int MIN = 1990;
+        int[] years = new int[5];
+        for (int i = 0; i < years.length; i++) {
+            years[i] = r.nextInt(MAX - MIN + 1) + MIN;
+            if(years[i]%2==1){
+                years[i]++;
+            }
+            System.out.printf("%d ",years[i]);
+        }
+        System.out.println("");
+    }
+    
+    public static void RandomCarPlate(Random r){
+        String plate = "";
+        int num = r.nextInt(99999-10000+1)+10000;
+        plate += num;
+        final int max = 26;
+        for(int i=0;i<2;i++){
+            plate += (char)(r.nextInt(max)+'A');
+        }
+        System.out.print(plate + "\n");
+    }
+    
+    public static void RandomWord(Random r){
+        String word = "";
+        final int max = 26;
+        char[] letter = {'a','A'};
+        for(int i=0;i<8;i++){
+            word += (char)(r.nextInt(max)+ letter[r.nextInt(2)]);
+        }
+        System.out.print(word + "\n");
+    }
+}
---
+++ PastYear2021/Q2.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pastyear_2022_1;
+
+/**
+ *
+ * @author Goh
+ */
+import java.util.*;
+import java.io.*;
+
+public class Q2 {
+
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(new FileInputStream("diving.txt"));
+            int n = 0;
+            while (sc.hasNextLine()) {
+                n++;
+                sc.nextLine();
+            }
+            sc.close();
+
+            int size = n / 5;
+            Diving[] divers = new Diving[size];
+
+            sc = new Scanner(new FileInputStream("diving.txt"));
+            for (int i = 0; i < divers.length; i++) {
+                String name = sc.nextLine();
+                String country = sc.nextLine();
+                double[][] scores = new double[3][7];
+                double[] difficulty = new double[3];
+                
+                for (int x = 0; x < scores.length; x++) {
+                    for (int j = 0; j < scores[x].length; j++) {
+                        scores[x][j] = sc.nextDouble();
+                    }
+                    difficulty[x] = sc.nextDouble();
+                }
+                divers[i] = new Diving(name, country, scores, difficulty);
+                if (i != divers.length - 1) {
+                    sc.nextLine();
+                }
+            }
+            sc.close();
+
+            for (Diving diver : divers) {
+                System.out.println(diver);
+                System.out.println("");
+            }
+            
+            //Bubble sort
+            for(int pass=1;pass<divers.length;pass++){
+                for(int i=0; i<divers.length-1;i++){
+                    if(divers[i].getFinal()<divers[i+1].getFinal()){
+                        Diving tmp = divers[i];
+                        divers[i] = divers[i+1];
+                        divers[i+1] = tmp;
+                    }
+                }
+            }
+            
+            System.out.printf("Gold : %s (%s)\n",divers[0].getName(),divers[0].getCountry()); 
+            System.out.printf("Silver : %s (%s)\n",divers[1].getName(),divers[1].getCountry()); 
+            System.out.printf("Bronze : %s (%s)\n",divers[2].getName(),divers[2].getCountry()); 
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+}
